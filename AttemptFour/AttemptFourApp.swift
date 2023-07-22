b89ab1fca9549b377215fb311a364e3ec41c71c7
LiaisanPerfilova <perliaisan@gmail.com>
2023-07-22 11:10:15 -0400

    Initial Commit

---
+++ AttemptFour/AttemptFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttemptFourApp.swift
+//  AttemptFour
+//
+//  Created by Liaisan on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AttemptFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
